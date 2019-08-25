cab01b16d404ccdf9c1d823c5cca9ba863f2b449
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-25 17:54:43 +0100

    Clarify naming in example.py

--- example.py
+++ example.py
@@ -1,14 +1,14 @@
 import coffeehouse
 
 api_key = "<API KEY>"
-api = coffeehouse.API(api_key)
+api_client = coffeehouse.API(api_key)
 
-session = api.create_session()
+session = api_client.create_session()
 print("Session ID: {0}".format(session.id))
 print("Session Available: {0}".format(str(session.available)))
 print("Session Language: {0}".format(str(session.language)))
 print("Session Expires: {0}".format(str(session.expires)))
 
 while(True):
-    output = api.think_thought(session.id, input("Input: "))
+    output = api_client.think_thought(session.id, input("Input: "))
     print("Output: {0}".format(output))
