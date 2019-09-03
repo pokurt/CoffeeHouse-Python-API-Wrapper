2c10ff2b1dd2032895acff618d721dca11f9fa5d
Netkas <netkas@users.noreply.github.com>
2019-09-03 11:15:37 -0400

    Bumped version to 1.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='coffeehouse',
-    version='1.0.3',
+    version='1.0.4',
     packages=['coffeehouse'],
     package_dir={
         'coffeehouse': 'coffeehouse'
