e27e99be0715d582c0d6b2760a07e3f5cd2a7460
netkas <netkas@intellivoid.info>
2020-02-18 15:04:22 -0300

    Updated examples

--- examples/lydia_example.py
+++ examples/lydia_example.py
@@ -1,27 +1,30 @@
 from coffeehouse.lydia import LydiaAI
 from coffeehouse.api import API
 
+# Create the CoffeeHouse API instance
 api_key = "<API KEY>"
 coffeehouse_api = API(api_key)
 
+# Create Lydia instance
 lydia = LydiaAI(coffeehouse_api)
 
+# Create a new chat session (Like a conversation)
 session = lydia.create_session()
 print("Session ID: {0}".format(session.id))
 print("Session Available: {0}".format(str(session.available)))
 print("Session Language: {0}".format(str(session.language)))
 print("Session Expires: {0}".format(str(session.expires)))
 
+# Talk to the bot!
 while True:
     output = session.think_thought(input("Input: "))
     print("Output: {0}".format(output))
 
-
 # In the case you want to save the Session ID to reuse the session
-# Use api_client to invoke think_thought instead, for example;
+# Use lydia to invoke think_thought instead, for example;
 #
 # while(True):
-#     output = api_client.think_thought(session.id, input("Input: "))
+#     output = lydia.think_thought(session.id, input("Input: "))
 #     print("Output: {0}".format(output))
 #
-# This is the same effect as above but uses the client directly.
+# This is the same effect as above but uses the lydia instance directly.
